1b916ebe7b3fc3a137c69ddc8a26be5ff6949cc8
mani29jan <mani29jan@yahoo.com>
2024-04-17 15:52:11 -0700

    Add files via upload

---
+++ dog/navbar.php
@@ -0,0 +1,47 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html>
+<title>Bay Area Labrador Retrievers</title>
+<link rel="stylesheet" href="css/stylesheet.css">
+<head>
+
+</head>
+<body class="background" background="img/bg.jpeg">
+	<!-- <body style="background-color:powderblue;"> -->
+
+	<ul class="navbar">
+		<li><a href="index.html">Home</a></li>
+		<li><a href="navbar/boy.html">Boy</a></li>
+		<li><a href="navbar/girl.html">Girl</a></li>
+		<li class="dropdown"><a href="#puppies">Puppies</a>
+			<div class="dropdown-content">
+				<a href="navbar/current_litters.html">Current Litters</a> <a
+					href="navbar/future_litters.html">Future Litters</a>
+			</div></li>
+		<li class="dropdown"><a href="#care">Care</a>
+			<div class="dropdown-content">
+				<a href="navbar/preparation.html">Preparation</a> <a
+					href="navbar/training.html">Training</a> <a
+					href="navbar/nutritions.html">Nutritions</a> <a
+					href="navbar/health.html">Health</a>
+			</div></li>
+		<li><a href="navbar/contact.html">Contact</a></li>
+	</ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
